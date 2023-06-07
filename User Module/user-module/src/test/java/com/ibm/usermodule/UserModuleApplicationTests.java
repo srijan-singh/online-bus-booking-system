79f52adb080c45693d99fd533fbcfcd2a2713052
srijan-singh <srijanssj5goku@gmail.com>
2023-06-07 18:11:14 +0530

    User Module

---
+++ User Module/user-module/src/test/java/com/ibm/usermodule/UserModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.usermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
