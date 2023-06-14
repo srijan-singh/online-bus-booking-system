91a74c0724f4e1547969cf0f947bec4d62e0ab4e
srijan-singh <srijanssj5goku@gmail.com>
2023-06-14 16:17:21 +0530

    Organized

--- User Module/booking-module/src/main/java/com/ibm/bookingmodule/repo/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.bookingmodule.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ibm.bookingmodule.entity.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-    
-}
--- User Module/booking-module/src/main/java/com/ibm/bookingmodule/repo/CancellationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.bookingmodule.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ibm.bookingmodule.entity.Cancellation;
-
-public interface CancellationRepository extends JpaRepository<Cancellation, Integer> {
-    
-}
