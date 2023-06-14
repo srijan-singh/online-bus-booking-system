fea72e8324d7880432261f9c17ff9fe48f6e1738
srijan-singh <srijanssj5goku@gmail.com>
2023-06-14 16:15:27 +0530

    Online Bus Booking System: API Gateway

---
+++ Backend/Server/online-bus-booking/src/main/java/com/ibm/onlinebusbooking/OnlineBusBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.onlinebusbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBusBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBusBookingApplication.class, args);
+	}
+
+}
---
+++ Backend/Server/online-bus-booking/src/test/java/com/ibm/onlinebusbooking/OnlineBusBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.onlinebusbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBusBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
