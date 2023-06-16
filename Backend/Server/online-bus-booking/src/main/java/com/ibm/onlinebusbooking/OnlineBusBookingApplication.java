3c7157ac7bef1317fe5ec730f7d4ba79c2301f69
srijan-singh <srijanssj5goku@gmail.com>
2023-06-16 09:26:59 +0530

    CrossOrigin: Supports Get Requests

--- Backend/Server/online-bus-booking/src/main/java/com/ibm/onlinebusbooking/OnlineBusBookingApplication.java
+++ Backend/Server/online-bus-booking/src/main/java/com/ibm/onlinebusbooking/OnlineBusBookingApplication.java
@@ -2,7 +2,9 @@ package com.ibm.onlinebusbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @SpringBootApplication
 public class OnlineBusBookingApplication {
 
