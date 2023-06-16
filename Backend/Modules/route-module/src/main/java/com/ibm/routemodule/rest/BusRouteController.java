fabf6bb20715d7f2a5f7f47aa9936dd01d84f0b4
srijan-singh <srijanssj5goku@gmail.com>
2023-06-16 21:06:43 +0530

    Feature: Enhanced

--- Backend/Modules/route-module/src/main/java/com/ibm/routemodule/rest/BusRouteController.java
+++ Backend/Modules/route-module/src/main/java/com/ibm/routemodule/rest/BusRouteController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ibm.routemodule.entity.BusRoute;
 import com.ibm.routemodule.service.BusRouteService;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class BusRouteController {
     
--- Backend/Modules/schedule-module/src/main/java/com/ibm/schedulemodule/rest/BusScheduleController.java
+++ Backend/Modules/schedule-module/src/main/java/com/ibm/schedulemodule/rest/BusScheduleController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/schedule")
 public class BusScheduleController {
