3df3dee47bee5162f0a90d7ace47ec572df303d2
srijan-singh <srijanssj5goku@gmail.com>
2023-06-16 21:08:27 +0530

    API Integrated : Route Module

--- Frontend/user-app/src/app/service/route.service.ts
+++ Frontend/user-app/src/app/service/route.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Route } from '../model/Route';
+import { RouteAPI } from './API';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteService {
 
-  constructor() { }
+  routes : Route[] = []
+
+  route : Route = new Route();
+
+  routeAPI : RouteAPI = new RouteAPI();
+
+  constructor(private http : HttpClient) { }
+
+  getRouteByID(routeID : number){
+    return this.http.get<Route>(this.routeAPI.getRouteByID(routeID));
+  }
+  
 }
