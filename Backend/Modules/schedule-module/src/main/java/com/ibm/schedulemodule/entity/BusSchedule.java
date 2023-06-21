148dc548f45058fe5a4379b0a4afd693bb19e778
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2023-06-21 10:14:40 +0530

    Comments added in Schedule

--- Backend/Modules/schedule-module/src/main/java/com/ibm/schedulemodule/entity/BusSchedule.java
+++ Backend/Modules/schedule-module/src/main/java/com/ibm/schedulemodule/entity/BusSchedule.java
@@ -1,6 +1,5 @@
 package com.ibm.schedulemodule.entity;
 
-
 import java.time.LocalDateTime;
 import javax.persistence.*;
 
@@ -10,35 +9,34 @@ public class BusSchedule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; // Unique ID for the bus schedule
 
     @Column(name = "departure_time")
-    private LocalDateTime departureTime;
+    private LocalDateTime departureTime; // Departure time of the bus schedule
 
     @Column(name = "arrival_time")
-    private LocalDateTime arrivalTime;
+    private LocalDateTime arrivalTime; // Arrival time of the bus schedule
 
     @Column(name = "route_id")
-    private Long routeID;
+    private Long routeID; // ID of the route associated with the bus schedule
     
     @Column(name = "amount")
-    private Double amount;
+    private Double amount; // Amount of the bus schedule
 
-	public BusSchedule() {
+    public BusSchedule() {
     }
 
     public BusSchedule(Long id, LocalDateTime departureTime, LocalDateTime arrivalTime,
-			Long routeID,Double amount) {
-		super();
-		this.id = id;
-		this.departureTime = departureTime;
-		this.arrivalTime = arrivalTime;
-		this.routeID = routeID;
-		this.amount = amount;
-	}
+            Long routeID, Double amount) {
+        super();
+        this.id = id;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.routeID = routeID;
+        this.amount = amount;
+    }
 
-
-	public Long getId() {
+    public Long getId() {
         return id;
     }
 
@@ -71,12 +69,10 @@ public class BusSchedule {
     }
     
     public Double getAmount() {
-		return amount;
-	}
+        return amount;
+    }
 
-	public void setAmount(Double amount) {
-		this.amount = amount;
-	}
-
-
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
--- Backend/Modules/schedule-module/src/main/java/com/ibm/schedulemodule/repository/BusScheduleRepository.java
+++ Backend/Modules/schedule-module/src/main/java/com/ibm/schedulemodule/repository/BusScheduleRepository.java
@@ -8,13 +8,16 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface BusScheduleRepository extends JpaRepository<BusSchedule,Long> {
-	
+public interface BusScheduleRepository extends JpaRepository<BusSchedule, Long> {
+
+    // Find bus schedules by route ID
     List<BusSchedule> findByRouteID(Long routeId);
-    
+
+    // Count the number of bus schedules with a specific arrival time and route ID
     @Query("SELECT COUNT(b) FROM BusSchedule b WHERE b.arrivalTime = :arrivalTime AND b.routeID = :routeID")
     int countByArrivalTimeAndRouteID(@Param("arrivalTime") LocalDateTime arrivalTime, @Param("routeID") Long routeID);
 
+    // Count the number of bus schedules with a specific departure time and route ID
     @Query("SELECT COUNT(b) FROM BusSchedule b WHERE b.departureTime = :departureTime AND b.routeID = :routeID")
     int countByDepartureTimeAndRouteID(@Param("departureTime") LocalDateTime departureTime, @Param("routeID") Long routeID);
 }
